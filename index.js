1e7eb259ad50b306856120012ee12ff071f18d6b
Kristiyan <kristiyan.atanasov@diva-e.com>
2025-10-20 19:51:49 +0100

    thank you page

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ document.getElementById('contactModal').onclick = function(e) {
     this.classList.remove('active');
   }
 };
+
+// Form submission with manual redirect
+const form = document.getElementById('contactForm');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const data = new FormData(form);
+  fetch(form.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      window.location.href = 'https://astroinsights.io/thank-you.html';
+    } else {
+      alert('Oops! There was a problem submitting your form');
+    }
+  }).catch(() => {
+    alert('Oops! There was a problem submitting your form');
+  });
+});
